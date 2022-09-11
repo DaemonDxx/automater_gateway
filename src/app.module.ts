6994bdcabc537b38e56b8e4d0b691ca315a4f8d6
DaemonDxx <i79535999238@gmail.com>
2022-09-11 11:51:18 +0700

    add config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import { validationSchema } from './utils/config/validation.schema';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [
+        '/etc/automater/production.env',
+        './config/production.env',
+        './config/development.env',
+      ],
+      validationSchema: validationSchema,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
