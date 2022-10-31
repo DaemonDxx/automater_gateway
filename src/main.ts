4840bb461db7820ae6e6a809be513e0e44121d4e
DaemonDxx <i79535999238@gmail.com>
2022-10-31 15:01:53 +0700

    fix: apply global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { Logger } from '@nestjs/common';
+import { GlobalValidationPipe } from './utils/pipes/global-validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = app.get<Logger>(WINSTON_MODULE_NEST_PROVIDER);
   app.useLogger(logger);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new GlobalValidationPipe({
+      whitelist: true,
+    }),
+  );
   app.enableCors();
   await app.listen(process.env.PORT);
 }
